034befa1ef15a732eea54d26ab270952f12e9d17
Abdur Rahman <152638668+abdurrahman482937@users.noreply.github.com>
2025-04-09 12:42:25 +0600

    ignoreDuringBuilds

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
-    domains: ["images.unsplash.com"]
-  }
+    domains: ["images.unsplash.com"],
+  },
 };
 
 export default nextConfig;
